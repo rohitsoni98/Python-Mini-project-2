56154d7e2f076902a33f7986594006ba5f148b7e
Rohit Kumar Soni <sonir973@gmail.com>
2021-05-17 00:30:24 +0530

    Updated

--- mini project2.py
+++ mini project2.py
@@ -2,21 +2,21 @@ import requests
 import json
 
 
-def s(str):
+def a(str):
     from win32com.client import Dispatch
 
-    s = Dispatch("SAPI.SpVoice")
+    a = Dispatch("SAPI.SpVoice")
 
-    s.Speak(str)
+    a.Speak(str)
 
 if __name__ == '__main__':
-    s("news for today.. Lets begin")
+    a("news for today.. Lets begin")
     url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=37e19dbce4cc4aaaa11e6d27d954cbdd"
     news = requests.get(url).text
     news_dict = json.loads(news)
     print(news_dict["articles"])
     arts = news_dict['articles']
     for article in arts:
-        s(article['title'])
-        s('moving on to the next news')
-    s('Thanks for listening')
+        a(article['title'])
+        a('moving on to the next news')
+    a('Thanks for listening')
